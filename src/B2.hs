ad9346bebad77ea2897ca034be887bf97ded76c0
Domen Kožar <domen@dev.si>
2020-01-29 05:29:28 +0100

    Add more retries and simplify

--- src/B2.hs
+++ src/B2.hs
@@ -876,10 +876,7 @@ retry delaysSeconds io = loop delaysSeconds
       liftIO $ threadDelay (floor $ delay * 1000 * 1000)
       loop delays
 
-retryThreeTimes :: IO a -> IO a
-retryThreeTimes = retry [0.1, 0.3, 0.5]
-
 dieW :: (Exception e) => IO (Either e a) -> IO a
-dieW x = retryThreeTimes $ do
-  res <- x
+dieW x = do
+  res <- retry [0.1, 0.2, 0.5, 1, 3, 10, 60] x
   either throwIO pure res
